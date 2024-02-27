a3764d8d7bc1f1b8dd0331d1cc9a20ba742679ba
Jorbbey <jorbbey.gmail.com>
2024-02-27 20:06:34 +0100

    commit

---
+++ src/components/Search/SearchResults.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+const SearchResults = ({isLoading, eventData}) => {
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default SearchResults
