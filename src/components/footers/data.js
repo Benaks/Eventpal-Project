7049f3eccde3acd09647be3d16c78e2a716bc84b
jorbbey <jorbbey@gmail.com>
2024-05-22 01:36:10 +0100

    remove redundant items

---
+++ src/components/footers/data.js
@@ -0,0 +1,38 @@
+const footerData = {
+ Eventrybe : [
+  "What is Eventrybe",
+  "What do we do",
+  "Why use Eventrybe",
+  "Eventrybe Pricing",
+  "Events we service",
+  "Faq",
+  "Eventrybe",
+],
+services : [
+  "Create events",
+  "Book events",
+  "Attend events",
+  "Our partners",
+  "Purchase ",
+  "Manage events",
+  "Find events",
+],
+help : [
+  "Your account",
+  "Cant attend events",
+  "Cant get tickets",
+  "Cant buy tickets",
+  "Terms and condition",
+  "Privacy and Policy",
+], 
+contactUs : [
+  "Facebook",
+  "Twitter",
+  "Instagram",
+  "Linkedin",
+  "Whatsapp",
+  "Contact with us",
+],
+}
+
+export default footerData;
