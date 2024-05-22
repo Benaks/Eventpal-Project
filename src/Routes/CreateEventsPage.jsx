ba2277ba25a6ce345e72350cd1301bef81887223
jorbbey <jorbbey@gmail.com>
2024-05-22 01:35:04 +0100

    fix state bug

--- src/Routes/CreateEventsPage.jsx
+++ src/Routes/CreateEventsPage.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Footer from "../components/footers/Footer";
 import Create from "../components/Create/Create";
 
 function CreateEventsPage() {
